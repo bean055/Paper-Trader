b05d4056022ddc9ebd6d8dae83cd29bad52627a7
bean055 <corey1aj@gmail.com>
2026-02-13 02:44:49 +0000

    fix on script mistake

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,11 +1,54 @@
+'use client';
+import { useEffect, useState } from "react";
 import "../../styles/pages/trade.css"
 import Navbar from "../components/Navbar"
+import "../../styles/global.css";
+import { useRouter } from "next/navigation";
+
+
 export default function Home() {
+  const [stocks, setStocks] = useState([]);
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    fetchTradePage();
+  },[page]);
+
+  const fetchTradePage = async () => {
+    const response = await fetch('/api/trades?page=${page}')
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Fetch failed");
+    }
+    const data = await response.json();
+    setStocks(data.stocks)
+  }
   return (
     <>
     <Navbar/>
     <div className="trade-page">
-      <h1 className="text-white text-4xl font-bold">Welcome to Paper Trader</h1>
+      <div className="Stocks-panel">
+        <h1> Stock list</h1>
+        <div className="stock-list-container">
+          {stocks.map((stocks) => (
+            <div key={stocks.asset_symbol} className="stock-item">
+              {stock.asset_symbol} - {stock.current_price}
+            </div>
+          ))}
+        </div>
+      </div>
+      
+      <div className ="watchlist-panel">
+
+      </div>
+
+      <div className="execution-panel">
+
+      </div>
+
+      <div className="info-panel">
+
+      </div>
     </div>
     </>
   );
